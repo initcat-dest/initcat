ca5f71289a1d9cbbddf04f2dad72a23a1cc5ea7e
libo <libo@xmiles.cn>
2019-03-24 16:02:29 +0800

    添加金币交易流水表 DBmodel

---
+++ user/user_service/src/main/java/com/initcat/user_service/model/db/CoinTransRecord.java
@@ -0,0 +1,44 @@
+package com.initcat.user_service.model.db;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * 金币交易流水表
+ *
+ * @author libo
+ * @package com.initcat.user_service.model.db
+ * @company xmiles
+ * @date 2019/3/24
+ */
+@Entity
+@Data
+public class CoinTransRecord {
+
+	@Id
+	private Long id;
+	/**
+	 * 用户ID
+	 */
+	private Long userId;
+	/**
+	 * 操作金币
+	 */
+	private Integer operateCoin;
+	/**
+	 * 操作类型 1：充值 ，2：消费
+	 */
+	private Integer operateType;
+	/**
+	 * 交易余额
+	 */
+	private Long tradeCoin;
+	/**
+	 * 创建时间
+	 */
+	private Date createTime;
+
+}
