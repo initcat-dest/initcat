4e9014395b82f2acfd3e8ae85d13486c5f48d16c
libo <libo@xmiles.cn>
2019-04-15 10:36:48 +0800

    签到功能添加TODO

--- user/user_common/src/main/java/com/initcat/user_common/model/enums/SignInfoEnum.java
+++ user/user_common/src/main/java/com/initcat/user_common/model/enums/SignInfoEnum.java
@@ -1,6 +1,6 @@
 package com.initcat.user_common.model.enums;
 
-
+// TODO song 为什么用SignInfoEnum命名
 public enum  SignInfoEnum{
     /**
      * 成功
@@ -8,19 +8,23 @@ public enum  SignInfoEnum{
     SUCCESS(1000, "成功"),
     /**
      * 请求参数非法
+     * TODO song 这个是否有用
      */
     PARAMETER_ILLEGAL(1001, "参数非法"),
     /**
      * 账户非法（不存在，或冻结）
+     * TODO song 这个是否有用
      */
     ACCOUNT_ILLEGAL(1002, "账户非法"),
 
     /**
      * 保存签到记录异常
+     * TODO song 这个为什么用在已签到的返回状态
      */
     SAVE_RECORD_ERROR(1004, "保存交易记录异常"),
     /**
      * 更新账户异常
+     * TODO song 这个是否有用
      */
     UPDATE_ACCOUNT_ERROR(1005, "更新账户异常"),
     /**
@@ -29,6 +33,7 @@ public enum  SignInfoEnum{
     REPEAT_REQUEST(1006, "重复请求"),
     /**
      * 服务异常（报错）
+     * TODO song 这个是否有用
      */
     SERVICE_ERROR(1007, "服务异常");
 
