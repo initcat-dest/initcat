5507eb59a9307df5a0294860e4e5ae642b32da72
Dest <destway@163.com>
2019-05-15 23:24:26 +0800

    Revert: 修改user-service启动类，兼容jenkins部署

--- user/user_service/src/main/java/com/initcat/user_service/UserServiceApplication.java
+++ user/user_service/src/main/java/com/initcat/user_service/UserServiceApplication.java
@@ -5,8 +5,6 @@ import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.alicp.jetcache.autoconfigure"})
@@ -14,12 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableMethodCache(basePackages = "com.initcat.user_service.dao")
 @EnableCreateCacheAnnotation
 @EnableDubboConfiguration
-public class UserServiceApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(UserServiceApplication.class);
-	}
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
