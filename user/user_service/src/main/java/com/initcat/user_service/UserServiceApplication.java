77d8251e9abfc0241df57137327cd6cfee1a629f
libo <libo@xmiles.cn>
2019-05-16 19:36:46 +0800

    修改启动类-修改tomcat依赖-兼容jenkins部署

--- user/user_service/src/main/java/com/initcat/user_service/UserServiceApplication.java
+++ user/user_service/src/main/java/com/initcat/user_service/UserServiceApplication.java
@@ -5,6 +5,8 @@ import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.alicp.jetcache.autoconfigure"})
@@ -12,10 +14,15 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableMethodCache(basePackages = "com.initcat.user_service.dao")
 @EnableCreateCacheAnnotation
 @EnableDubboConfiguration
-public class UserServiceApplication {
+public class UserServiceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UserServiceApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
 }
